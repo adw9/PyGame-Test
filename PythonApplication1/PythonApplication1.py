04655f0b39dc6a5d76e7ccf49047f27cddcf7025
adw9 <adw9@clemson.edu>
2023-01-18 13:57:02 -0500

    removing the ghost !

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -5,7 +5,6 @@ BLACK = ( 0, 0, 0)
 WHITE = ( 255, 255, 255)
 GREEN = ( 0, 255, 0)
 RED = ( 255, 0, 0)
-!
 size = (100, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My First PyGame!")
